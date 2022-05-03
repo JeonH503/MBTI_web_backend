9b31c10a2939ae3823b2cb7fdd64d479973f3b9b
onlyplsson <onlyplsson@gmail.com>
2022-05-03 11:24:44 +0900

    작업한 내용 저장

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql');
-const fs = require('fs');
-const data = fs.readFileSync('./database.json');
+const mysql = require("mysql");
+const fs = require("fs");
+const data = fs.readFileSync("./database.json");
 const conf = JSON.parse(data);
 
 const connection = mysql.createConnection({
-    host : conf.host,
-    user : conf.user,
-    password : conf.password,
-    database : conf.database
+  host: conf.host, //54.180.122.20
+  user: conf.user,
+  password: conf.password,
+  database: conf.database,
 });
 
 module.exports = connection;
