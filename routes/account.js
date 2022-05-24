f1827b8c78fba5930c35b0980940e5679cb317bd
jjh <wjs4222@naver.com>
2022-05-24 11:33:16 +0900

    cors제한 해제 계정 수정 추가중

--- routes/account.js
+++ routes/account.js
@@ -50,13 +50,43 @@ router.post("/", function (req, res) {
 
 router.patch("/:id", async function (req, res) {
   try {
+    let id = String(req.params.id);
+    let password = String(req.body.password);
+    let mbti = String(req.body.mbti);
+    let salt = "";
+
+    if(!id && !password && !mbti)
+        res.status(400).send("잘못된 형식입니다")
+
+    crypto.randomBytes(64, (err, buf) => {
+        crypto.pbkdf2(password, buf.toString('base64'), 100000, 64, 'sha512', (err, key) => {
+            let incoded_password = key.toString('base64');
+            salt = buf.toString('base64');
+            let sql = `INSERT INTO account(id, mbti, salt, password) VALUES ('${id}','${mbti}','${salt}','${incoded_password}')`
+            let sql = `
+            update
+            from account
+            set mbti=${mbti},
+            salt=${salt},
+            passsword=${incoded_password}
+            `
+            conn.query(sql, (err, rows, fields) => {
+                if(err) {
+                    res.status(400).send({ err })
+                }
+
+                res.status(201).send("ok");
+            })
+        });
+    });  
   } catch (err) {
-    res.status(400).send({ err: "잘못된 형식 입니다." });
+      res.status(400).send({ err });
   }
 });
 
 router.delete("/:id", async function (req, res) {
   try {
+    
   } catch (err) {
     res.status(400).send({ err: "잘못된 형식 입니다." });
   }
