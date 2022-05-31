8bc9fe2dc72d5b7c2ad6849a267cd002f0f7aa84
jjh <wjs4222@naver.com>
2022-05-31 12:18:59 +0900

    이미지 업로드 기능 추가

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const crypto = require("crypto");
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const bodyParser = require("body-parser");
-``;
+const multer = require('multer');
 const jsonwebtoken = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,8 +18,9 @@ const helmet = require("helmet");
 const cors = require('cors');
 
 app.use(cors())
-
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,6 +29,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/account", accountRouter);
 app.use("/comment", commentRouter);
 app.use("/post", postRouter);
+
+// static 설정
+app.use('/uploads',express.static('./uploads'))
+
+// multer 설정
+const upload = multer({
+  dest:"uploads/images"
+})
 
 //인증 미들웨어
 app.use(function (req, res, next) {
@@ -51,7 +60,8 @@ app.use(function (req, res, next) {
   } else if (
         (req.method !== "GET" && req.path === '/account') ||
         req.method === "GET" || 
-        req.path === "/login" || 
+        req.path === "/login" ||
+        (req.method === "GET" && req.path === '/uploads') ||
         (req.path === "/account" && req.method === "POST")
     ) {
     next();
@@ -113,5 +123,19 @@ app.post("/login", async function (req, res) {
     res.status(400).send({ err: "잘못된 형식 입니다." });
   }
 });
+
+
+// uploads API
+app.post('/upload/image',upload.single('upload'), function(req, res){
+  const image_path = req.file.path
+  
+  if(!image_path) {
+    res.status(400).send({
+      error:'이미지가 존재하지 않습니다.'
+    })
+  }
+
+  res.status(200).send({url:"http://54.180.122.20:3000/"+image_path})
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
